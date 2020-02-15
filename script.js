5bfbfe34a8e13c9f8e418a14b8978985a67d032d
Sydney Krantz <syd107@gmail.com>
2020-02-15 10:16:50 -0800

    merging from master branch

--- script.js
+++ script.js
@@ -1,83 +1,74 @@
 var zipcode = $("#searchBar").val();
 
-$("#searchButton").click(function(e){
-
+$("#searchButton").click(function(e) {
   displayDadJoke();
 
-  console.log("clicked the search button GO")
-  var zipcode = $("#searchBar").val().trim();
+  console.log("clicked the search button GO");
+  var zipcode = $("#searchBar")
+    .val()
+    .trim();
   console.log(zipcode);
   getBreweries(zipcode);
   e.preventDefault();
 
+  //!!!!!!!!!!!!!!!!!!!
 
+  // create the on click listener
+  // grab the zip from the input .val().trim()
+  // pass that zip to your getBreweries(zip) function
+  // $("#submit-btn").on("click", function(e) {
+  // e.preventDefault();
+  // var zipCode = $('#zip').val().trim();
+  // getBreweries(zipCode);
 
-//!!!!!!!!!!!!!!!!!!!
+  // })
+  // const getBreweries = zipCode => {
+  //   $.ajax({
+  //     url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=brewery&location=${zipCode}`,
+  //     method: "GET",
+  //     headers: {
+  //       'Authorization':
+  //         "Bearer bvfT3iWDcpR-4Muk889vmNcivjsGMRCxzB3k1TagHYfLTnUunGHZq5fWVIOSNm6L5dwwzoXTsBmkU42-YT353irMrVF8kdd_kGAUSHLc-0dnINfstgnOCmw7qC0_XnYx",
+  //       'Access-Control-Allow-Origin': '*',
+  //       'Access-Control-Allow-Credentials': true,
+  //     }
+  //   })
+  //     .then(function(response) {
+  //       // do something with the response
+  //       console.log(response)
+  //     })
+  //     .catch(function(e) {
+  //       console.log(e);
+  //     });
+  // };
 
-
-
-
-      // create the on click listener
-      // grab the zip from the input .val().trim()
-      // pass that zip to your getBreweries(zip) function
-      // $("#submit-btn").on("click", function(e) {
-        // e.preventDefault();
-        // var zipCode = $('#zip').val().trim();
-        // getBreweries(zipCode);
-      
-      // })
-      // const getBreweries = zipCode => {
-      //   $.ajax({
-      //     url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=brewery&location=${zipCode}`,
-      //     method: "GET",
-      //     headers: {
-      //       'Authorization':
-      //         "Bearer bvfT3iWDcpR-4Muk889vmNcivjsGMRCxzB3k1TagHYfLTnUunGHZq5fWVIOSNm6L5dwwzoXTsBmkU42-YT353irMrVF8kdd_kGAUSHLc-0dnINfstgnOCmw7qC0_XnYx",
-      //       'Access-Control-Allow-Origin': '*',
-      //       'Access-Control-Allow-Credentials': true,
-      //     }
-      //   })
-      //     .then(function(response) {
-      //       // do something with the response
-      //       console.log(response)
-      //     })
-      //     .catch(function(e) {
-      //       console.log(e);
-      //     });
-      // };
-
-
-
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-})
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!
+});
 const getBreweries = zipcode => {
   $.ajax({
     url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=brewery&location=${zipcode}`,
     method: "GET",
     headers: {
-      'Authorization':
+      Authorization:
         "Bearer bvfT3iWDcpR-4Muk889vmNcivjsGMRCxzB3k1TagHYfLTnUunGHZq5fWVIOSNm6L5dwwzoXTsBmkU42-YT353irMrVF8kdd_kGAUSHLc-0dnINfstgnOCmw7qC0_XnYx",
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
     }
   })
     .then(function(response) {
       console.log(response);
       // do something with the response
-      console.log(response.businesses[0].alias)
+      console.log(response.businesses[0].alias);
       $("#listItem0").text(response.businesses[0].name);
-      console.log(response.businesses[1].name)
+      console.log(response.businesses[1].name);
       $("#listItem1").text(response.businesses[1].name);
-      console.log(response.businesses[2].name)
+      console.log(response.businesses[2].name);
       $("#listItem2").text(response.businesses[2].name);
     })
     .catch(function(e) {
       console.log(e);
     });
 };
-
 
 // THIS IS ALL THE STUFF PERTAINING TO THE GOOGLE MAP DISPLAY YO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // reference to the map in html
@@ -86,7 +77,7 @@ var map = document.getElementById("mapDisplay");
 
 // This runs first
 function init() {
-  console.log("initialized the script...")
+  console.log("initialized the script...");
   document.getElementById("mapDisplay").textContent = "Loading ...";
   // calls getLocation
   getLocation();
@@ -123,12 +114,11 @@ var breweries = [
   }
 ];
 
-
 function getLocation() {
   //   get location checks to see if you allow geolocation
   if (navigator.geolocation) {
     // if so, let's find the user's current position
-    console.log("eagle mode. ")
+    console.log("eagle mode. ");
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
     console.log("no geolocation allowed");
@@ -153,7 +143,7 @@ function initMap(lat, lon) {
   map = new google.maps.Map(document.getElementById("mapDisplay"), {
     zoom: 13,
     //   set center to appropriate place
-    center: { lat, lng: lon },
+    center: { lat, lng: lon }
     // mapTypeId: "terrain"
   });
 
@@ -162,10 +152,7 @@ function initMap(lat, lon) {
   // set of coordinates.
   breweries.forEach(brewery => {
     console.log(brewery);
-    var latLng = new google.maps.LatLng(
-      brewery.city.lat,
-      brewery.city.lon
-    );
+    var latLng = new google.maps.LatLng(brewery.city.lat, brewery.city.lon);
     var marker = new google.maps.Marker({
       position: latLng,
       map: map,
@@ -182,7 +169,9 @@ function initMap(lat, lon) {
 //THIS CONCLUDES ALL THE STUFF PERTAINING TO THE MAP DISPLAY API !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 //dad joke code..
-function displayDadJoke(){
-  console.log("and also here's a random dad joke... LOL")
-  $("#dadJokes").text("I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!")
+function displayDadJoke() {
+  console.log("and also here's a random dad joke... LOL");
+  $("#dadJokes").text(
+    "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!"
+  );
 }
